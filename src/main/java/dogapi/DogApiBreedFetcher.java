7040f118b4c0cdd684c3a9c74cc749c30062a525
Ale Benavides Loo <albenaloo27@gmail.com>
2025-10-23 19:43:08 -0400

    TODO 1

--- src/main/java/dogapi/DogApiBreedFetcher.java
+++ src/main/java/dogapi/DogApiBreedFetcher.java
@@ -19,17 +19,36 @@ public class DogApiBreedFetcher implements BreedFetcher {
 
     /**
      * Fetch the list of sub breeds for the given breed from the dog.ceo API.
+     *
      * @param breed the breed to fetch sub breeds for
      * @return list of sub breeds for the given breed
      * @throws BreedNotFoundException if the breed does not exist (or if the API call fails for any reason)
      */
     @Override
     public List<String> getSubBreeds(String breed) {
-        // TODO Task 1: Complete this method based on its provided documentation
-        //      and the documentation for the dog.ceo API. You may find it helpful
-        //      to refer to the examples of using OkHttpClient from the last lab,
-        //      as well as the code for parsing JSON responses.
         // return statement included so that the starter code can compile and run.
-        return new ArrayList<>();
+
+        String link = "https://dog.ceo/api/breed/"+breed+"/list";
+        Request request = new Request.Builder()
+                .url(link)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            final JSONObject responseBody = new JSONObject(response.body().string());
+            if (Objects.equals(responseBody.getString("status"), "error")) {
+                throw new BreedNotFoundException(breed);
+            }
+            else {
+                ArrayList<String> list_breeds = new ArrayList<>();
+                JSONArray breeds = responseBody.getJSONArray("message");
+                for (int i = 0; i < breeds.length(); i++) {
+                    list_breeds.add(breeds.getString(i));
+                }
+                return list_breeds;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new ArrayList<>();
+        }
     }
 }
